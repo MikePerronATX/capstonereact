aa0027c0fab3a29d931cc2e31af99b176cb99dd9
Michael Perron <mikeperron.atx@gmail.com>
2022-06-10 19:04:25 -0500

    Sign in page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/routes/navigation/Navbar';
-
 import Home from './components/routes/home/Home';
+import SignIn from './components/routes/sign-in/Sign-in';
 
 const Shop = () => {
   return <h1> Shop</h1>;
@@ -14,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navbar />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
