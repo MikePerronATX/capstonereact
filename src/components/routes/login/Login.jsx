56775fd275b3651c36fe486f3dcacd954433c8ef
Michael Perron <mikeperron.atx@gmail.com>
2022-06-15 08:07:43 -0500

    Adjusting Firebase, and adding Signup form.

--- src/components/routes/login/Login.jsx
+++ src/components/routes/login/Login.jsx
@@ -3,19 +3,20 @@ import {
   createUserDocumentFromAuth
 } from '../../../utils/firebase/Firebase'
 
+import SignUpForm from '../../sign-up-form/SignUpForm';
+
 const Login = () => {
   const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Login Page</h1>
-      <button onClick={logGoogleUser}>
-        Sign in with Google Popup
-      </button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
-  )
-}
+  );
+};
 export default Login;
